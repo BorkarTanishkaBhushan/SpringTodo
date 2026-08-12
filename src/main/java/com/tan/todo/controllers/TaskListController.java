6aaff15a3b973b90f3a9178b0df8f1958a505315
tanishkaBBR <tanishka.b@somaiya.edu>
2026-08-12 07:20:19 +0530

    Get Task List by ID endpoint

--- src/main/java/com/tan/todo/controllers/TaskListController.java
+++ src/main/java/com/tan/todo/controllers/TaskListController.java
@@ -5,12 +5,11 @@ import com.tan.todo.domain.entites.TaskList;
 import com.tan.todo.mappers.TaskListMapper;
 import com.tan.todo.services.TaskListService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 @RequestMapping(path = "/task-lists")
@@ -26,7 +25,7 @@ public class TaskListController {
     }
 
     @GetMapping
-    public List<TaskListDto> listTaskLists(){
+    public List<TaskListDto> listTaskLists() {
         return taskListService.listTaskLists()
                 .stream()
                 .map(taskListMapper::toDto)
@@ -34,10 +33,16 @@ public class TaskListController {
     }
 
     @PostMapping
-    public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto){
+    public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto) {
         TaskList createdTaskList = taskListService.createTaskList(
                 taskListMapper.fromDto(taskListDto)
         );
         return taskListMapper.toDto(createdTaskList);
     }
+
+    @GetMapping(path = "/{task_list_id}")
+    public Optional<TaskListDto> getTaskList(@PathVariable("task_list_id") UUID taskListId){
+        return taskListService.getTaskList(taskListId).map(taskListMapper::toDto);
+    }
+
 }
--- src/main/java/com/tan/todo/services/TaskListService.java
+++ src/main/java/com/tan/todo/services/TaskListService.java
@@ -3,8 +3,11 @@ package com.tan.todo.services;
 import com.tan.todo.domain.entites.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 }
